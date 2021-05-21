5609506cc7cb870d919cdd117c02c5565826914d
José Augusto Thomas <thomas.joseaugusto@gmail.com>
2021-05-21 09:42:47 -0300

    Fix warning about missing virtual dctor in simple_exponential_indexer

--- src/simple_exponential_indexer.h
+++ src/simple_exponential_indexer.h
@@ -11,6 +11,6 @@ namespace core {
             simple_exponential_indexer(steady_clock::time_point series_start_date, double year_growth);
             double evaluate_at(const steady_clock::time_point& time_point) const override;
             double get_rate_delta(const steady_clock::time_point& from, const steady_clock::time_point& to) const override;
-            ~simple_exponential_indexer();
+            virtual ~simple_exponential_indexer();
     };
 }
